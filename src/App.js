f77e10d86e557c19d7c4e9da8a2aec0d3a8accb7
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-20 14:23:40 -0700

    app header and structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Projects from "./Projects";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Xunaxidonita</h1>
+        <menu>
+          <a href="#1">About me</a>
+          <a href="#2">Portfolio</a>
+          <a href="#3">Contact</a>
+          <a href="#3">Resume</a>
+        </menu>
       </header>
+      <main>
+        <Projects></Projects>
+      </main>
     </div>
   );
 }
---
+++ src/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section>
+      <h1 id="projects">Projects</h1>
+    </section>
+  );
+}
+
+export default Projects;
