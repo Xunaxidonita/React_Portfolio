e5cde7a2e176b2be5a6c7f5ee1fc5256aeb0909e
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-07-22 12:57:39 -0700

    About me ready

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Image } from "react-router-dom";
 
 const About = () => (
   <>
     <Header></Header>
+    <h2>About me</h2>
+    <Image
+      src="./assets/images/Donita.png"
+      alt="Donaji Velazquez's profile picture"
+    ></Image>
+    <h4>Maria Donaji Velazquez Orduna</h4>
+    <h5>Graphic Designer/Programer</h5>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+
     <Footer></Footer>
   </>
 );
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,31 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
+const projects = [
+    {key:"", value:"", }
+]
+
 export default MainPage;
-const MainPage = () => (
-    let { _id } = useParams();
-  
-    const { loading, data: userData } = useQuery(QUERY_ME);
-  
-    const { loadingProject, data: projectData } = useQuery(PROJECT, {
-      variables: { _id },
-    });
-  
-    const projects = userData?.me?.projects;
-  
-    return (
+const MainPage = (projects) => (
       <>
         <Header></Header>
-        <ProjectViewer projects={projects} loading={loading}>
-          <ProjectCard project={projectData?.project}></ProjectCard>
+        <ProjectViewer projects>
+          <ProjectCard project></ProjectCard>
         </ProjectViewer>
         <Footer></Footer>
       </>
-    );
-  <>
-    
-    
-  </>
+
 );
 
 export default MainPage;
